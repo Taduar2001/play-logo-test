a56f97cd48f101fc9e2d6e11ebc866e401ce76cc
Alex <92122843+Taduar2001@users.noreply.github.com>
2024-09-28 21:25:06 +0300

    Add files via upload

---
+++ __settings__.js
@@ -0,0 +1,22 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "2062760.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'deviceTypes': [`webgl2`, `webgl1`],
+    'powerPreference': "default"
+};
+window.SCRIPTS = [ 194394659, 194394660, 194394658, 197621819, 197621865, 197621866, 197646058, 197646059, 197646064, 197646066, 197646068, 197646072, 197646073, 197646082, 197646085, 197646087, 197646184 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/194394677/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/194394681/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/194394672/1/ammo.js', 'preload' : true},
+];
