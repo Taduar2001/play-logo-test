a7a0a33f4094029646e4478febeb5eae2f85504f
Alex <voronko20172017@gmail.com>
2024-10-10 11:59:21 +0300

    455

--- __settings__.js
+++ __settings__.js
@@ -8,7 +8,7 @@ window.CONTEXT_OPTIONS = {
     'deviceTypes': [`webgl2`, `webgl1`],
     'powerPreference': "default"
 };
-window.SCRIPTS = [ 197621819, 197621865, 197621866, 197646058, 197646059, 197646064, 197646066, 197646068, 197646072, 197646073, 197646082, 197646085, 197646087, 197646184, 197820484, 198067709, 198380295, 198961879, 198995531, 199020890, 199021131, 199025170, 199025593 ];
+window.SCRIPTS = [ 197621819, 197621865, 197621866, 197646058, 197646059, 197646064, 197646066, 197646068, 197646072, 197646073, 197646082, 197646085, 197646087, 197646184, 197820484, 198067709, 198380295, 198961879, 198995531, 199020890, 199021131, 199025170, 199025593, 199181465 ];
 window.CONFIG_FILENAME = "config.json";
 window.INPUT_SETTINGS = {
     useKeyboard: true,
@@ -18,5 +18,6 @@ window.INPUT_SETTINGS = {
 };
 pc.script.legacy = false;
 window.PRELOAD_MODULES = [
+    {'moduleName' : 'DracoDecoderModule', 'glueUrl' : 'files/assets/199190764/1/draco.wasm.js', 'wasmUrl' : 'files/assets/199190765/1/draco.wasm.wasm', 'fallbackUrl' : 'files/assets/199190763/1/draco.js', 'preload' : true},
     {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/194394677/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/194394681/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/194394672/1/ammo.js', 'preload' : true},
 ];
